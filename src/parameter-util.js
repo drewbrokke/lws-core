b9cee958a9afd94c573a494c298dc4440b211884
drewbrokke <drew.brokke@liferay.com>
2016-10-26 15:26:54 -0700

    adds getBasicTypeValue function to parameter-util.js

--- src/parameter-util.js
+++ src/parameter-util.js
@@ -1,5 +1,30 @@
 /* @flow */
 
+
+export function getBasicTypeValue(type: string): any {
+	if (type.includes('[]') ||
+		type.includes('util.List')) {
+		return null;
+	}
+
+	if (type.includes('Map')) {
+		return JSON.stringify({});
+	}
+
+	if (type.includes('long') ||
+		type.includes('int') ||
+		type.includes('short') ||
+		type.includes('double')) {
+		return  0;
+	}
+
+	if (type.includes('boolean')) {
+		return false;
+	}
+
+	return '';
+}
+
 export function zipObjectFromArrays(keys: string[], values: string[]): {[index:string]: string} {
 	return keys.reduce((prev, cur, i) => {
 			prev[cur] = values[i];
