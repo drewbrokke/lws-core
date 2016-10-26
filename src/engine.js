bfafd6e4755f63174f6e5d3f9aa95ca6eb9248a9
drewbrokke <drew.brokke@liferay.com>
2016-10-26 12:16:26 -0700

    Uses native http/https modules to make requests

--- src/engine.js
+++ src/engine.js
@@ -1,46 +1,68 @@
 /* @flow */
 
+import http from 'http';
+import https from 'https';
 import path from 'path';
-import {post} from 'request';
+import querystring from 'querystring';
 
-import {constructBasePath, prependProtocol} from './path-util';
+import validateNumber from './validate-number';
 
+import type {IncomingMessage as HttpIncomingMessage} from 'http';
+import type {IncomingMessage as HttpsIncomingMessage} from 'https';
 import type {InstanceConfig} from './types';
 
 export default class Engine {
-	username: string;
-	password: string;
-	secure: boolean;
-	basePath: string;
+	instanceConfig: InstanceConfig;
 
 	constructor(instanceConfig: InstanceConfig) {
-		this.username = instanceConfig.username;
-		this.password = instanceConfig.password;
-		this.secure = instanceConfig.secure;
-
-		this.basePath = constructBasePath(instanceConfig.host, instanceConfig.port);
+		this.instanceConfig = instanceConfig;
 	}
 
 	invoke(apiPath: string, payload: Object): Promise<any> {
-		const fullPath: string = prependProtocol(
-			path.join(this.basePath, apiPath), this.secure);
+		const payloadString: string = querystring.stringify(payload);
+		const port: number = this.instanceConfig.port;
 
-		const form: {form: Object} = {form: payload};
+		const requestOptions = {
+			auth: `${this.instanceConfig.username}:${this.instanceConfig.password}`,
+			host: this.instanceConfig.host,
+			method: 'POST',
+			path: path.join('/api/jsonws', apiPath),
+			port: this.instanceConfig.secure ? 443 : 80,
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Length': Buffer.byteLength(payloadString)
+			}
+		};
+
+		if (validateNumber(port) && port != 443 && port != 80) {
+			requestOptions['port'] = Number(this.instanceConfig.port);
+		}
 
 		return new Promise((resolve, reject) => {
-			function handleResponse(err: Error, response: Object, body: string): void {
-				if (err) {
-					return reject(err);
+			function handleResponse(response: HttpsIncomingMessage | HttpIncomingMessage): void {
+				let body = '';
+
+				const handleResponseEnd = () => {
+					if (response.statusCode != 200) {
+						return reject(body);
+					}
+
+					return resolve(body);
+
 				}
 
-				if (response.statusCode !== 200) {
-					return reject(new Error(body));
-				}
-
-				return resolve(JSON.parse(body));
+				response.on('data', (d: string) => body += d);
+				response.on('end', handleResponseEnd);
 			}
 
-			return post(fullPath, form, handleResponse).auth(this.username, this.password);
+			const request = this.instanceConfig.secure
+				? https.request(requestOptions, handleResponse)
+				: http.request(requestOptions, handleResponse);
+
+			request.on('error', (err: Error) => reject(err));
+
+			request.write(payloadString);
+			request.end();
 		});
 	}
 }
