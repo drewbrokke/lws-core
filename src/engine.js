5a18bb7df70bd95ff161a119dee2c3a8f96f715f
drewbrokke <drew.brokke@liferay.com>
2016-10-29 17:24:24 -0700

    moves request logic to new http-util.js

--- src/engine.js
+++ src/engine.js
@@ -1,14 +1,7 @@
 /* @flow */
 
-import http from 'http';
-import https from 'https';
-import path from 'path';
-import querystring from 'querystring';
+import {get, post} from './http-util.js';
 
-import validateNumber from './validate-number';
-
-import type {IncomingMessage as HttpIncomingMessage} from 'http';
-import type {IncomingMessage as HttpsIncomingMessage} from 'https';
 import type {InstanceConfig, RequestOptions} from './types';
 
 export default class Engine {
@@ -19,55 +12,10 @@ export default class Engine {
 	}
 
 	invoke(apiPath: string, payload: Object): Promise<any> {
-		const requestOptions: RequestOptions = Engine.constructPostRequestOptions(apiPath, payload, this.instanceConfig);
-
-		return new Promise((resolve, reject) => {
-			function handleResponse(response: HttpsIncomingMessage | HttpIncomingMessage): void {
-				let body = '';
-
-				const handleResponseEnd = () => {
-					if (response.statusCode != 200) {
-						return reject(body);
-					}
-
-					return resolve(body);
-
-				}
-
-				response.on('data', (d: string) => body += d);
-				response.on('end', handleResponseEnd);
-			}
-
-			const request = this.instanceConfig.secure
-				? https.request(requestOptions, handleResponse)
-				: http.request(requestOptions, handleResponse);
-
-			request.on('error', (err: Error) => reject(err));
-			request.write(querystring.stringify(payload));
-			request.end();
-		});
+		return post(apiPath, payload, this.instanceConfig);
 	}
 
-	static constructPostRequestOptions(
-		apiPath: string, payload: Object, instanceConfig: InstanceConfig): RequestOptions {
-		const payloadString: string = querystring.stringify(payload);
-		const port: number | void = instanceConfig.port;
-
-		const requestOptions: RequestOptions = {
-			auth: `${instanceConfig.username}:${instanceConfig.password}`,
-			host: instanceConfig.host,
-			method: 'POST',
-			path: path.join('/api/jsonws', apiPath),
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': Buffer.byteLength(payloadString)
-			}
-		};
-
-		if (validateNumber(port) && port != 443 && port != 80) {
-			requestOptions['port'] = Number(instanceConfig.port);
-		}
-
-		return requestOptions;
+	getHTML(address: string): Promise<any> {
+		return get(address, this.instanceConfig);
 	}
 }
---
+++ src/http-util.js
@@ -0,0 +1,115 @@
+/* @flow */
+
+import http from 'http';
+import https from 'https';
+import path from 'path';
+import querystring from 'querystring';
+import url from 'url';
+
+import validateNumber from './validate-number';
+import type {IncomingMessage as HttpIncomingMessage} from 'http';
+import type {IncomingMessage as HttpsIncomingMessage} from 'https';
+
+import type {InstanceConfig, RequestOptions} from './types';
+
+export function get(address: string, instanceConfig: InstanceConfig): Promise<any> {
+	const requestOptions: RequestOptions = constructGetRequestOptions(address, instanceConfig);
+
+	return new Promise((resolve, reject) => {
+		function handleResponse(response: HttpsIncomingMessage | HttpIncomingMessage): void {
+			let body = '';
+
+			const handleResponseEnd = () => {
+				if (response.statusCode != 200) {
+					return reject(body);
+				}
+
+				return resolve(body);
+
+			}
+
+			response.on('data', (d: string) => body += d);
+			response.on('end', handleResponseEnd);
+		}
+
+		const request = instanceConfig.secure
+			? https.request(requestOptions, handleResponse)
+			: http.request(requestOptions, handleResponse);
+
+		request.on('error', (err: Error) => reject(err));
+		request.end();
+	});
+}
+
+export function post(apiPath: string, payload: Object, instanceConfig: InstanceConfig): Promise<any> {
+	const requestOptions: RequestOptions = constructPostRequestOptions(apiPath, payload, instanceConfig);
+
+	return new Promise((resolve, reject) => {
+		function handleResponse(response: HttpsIncomingMessage | HttpIncomingMessage): void {
+			let body = '';
+
+			const handleResponseEnd = () => {
+				if (response.statusCode != 200) {
+					return reject(body);
+				}
+
+				return resolve(body);
+
+			}
+
+			response.on('data', (d: string) => body += d);
+			response.on('end', handleResponseEnd);
+		}
+
+		const request = instanceConfig.secure
+			? https.request(requestOptions, handleResponse)
+			: http.request(requestOptions, handleResponse);
+
+		request.on('error', (err: Error) => reject(err));
+		request.write(querystring.stringify(payload));
+		request.end();
+	});
+}
+
+function constructGetRequestOptions(
+	address: string, instanceConfig: InstanceConfig): RequestOptions {
+	const port: number | void = instanceConfig.port;
+
+	const urlPath: string = url.parse(address).path || '/';
+
+	const requestOptions: RequestOptions = {
+		auth: `${instanceConfig.username}:${instanceConfig.password}`,
+		host: instanceConfig.host,
+		method: 'GET',
+		path: urlPath
+	};
+
+	if (validateNumber(port) && port != 443 && port != 80) {
+		requestOptions['port'] = Number(instanceConfig.port);
+	}
+
+	return requestOptions;
+}
+
+function constructPostRequestOptions(
+	apiPath: string, payload: Object, instanceConfig: InstanceConfig): RequestOptions {
+	const payloadString: string = querystring.stringify(payload);
+	const port: number | void = instanceConfig.port;
+
+	const requestOptions: RequestOptions = {
+		auth: `${instanceConfig.username}:${instanceConfig.password}`,
+		host: instanceConfig.host,
+		method: 'POST',
+		path: path.join('/api/jsonws', apiPath),
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': Buffer.byteLength(payloadString)
+		}
+	};
+
+	if (validateNumber(port) && port != 443 && port != 80) {
+		requestOptions['port'] = Number(instanceConfig.port);
+	}
+
+	return requestOptions;
+}
