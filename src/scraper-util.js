81fff2e3851283fe4c1d928e98165460a21c8b83
drewbrokke <drew.brokke@liferay.com>
2016-11-01 10:15:52 -0700

    scraper-util - makes static class and reduces calls for getMainScraper

--- src/scraper-util.js
+++ src/scraper-util.js
@@ -3,20 +3,20 @@
 import Engine from './engine';
 import MainScraper from './scraper/main-scraper';
 import MethodScraper from './scraper/method-scraper';
-import type {InstanceConfig} from './types';
 
-export async function getMainScraper(engine: Engine): Promise<MainScraper> {
-	const rootHtml: string = await engine.getRootHTML();
+export default class ScraperUtil {
+	static async getMainScraper(engine: Engine): Promise<MainScraper> {
+		const rootHtml: string = await engine.getRootHTML();
 
-	return new MainScraper(rootHtml);
+		return new MainScraper(rootHtml);
+	}
+
+	static async getMethodScraper(methodName: string, mainScraper: MainScraper, engine: Engine): Promise<MethodScraper> {
+		const urls: string[] = mainScraper.getMethodURLs(methodName);
+
+		const methodHtml: string = await engine.getHTML(urls[0]);
+
+		return new MethodScraper(methodHtml);
+	}
 }
 
-export async function getMethodScraper(methodName: string, engine: Engine): Promise<MethodScraper> {
-	const mainScraper: MainScraper = await getMainScraper(engine);
-
-	const urls: string[] = mainScraper.getMethodURLs(methodName);
-
-	const methodHtml: string = await engine.getHTML(urls[0]);
-
-	return new MethodScraper(methodHtml);
-}
