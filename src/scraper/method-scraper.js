2bb63de6e59a9de2673e5dba293c60afd60477b8
drewbrokke <drew.brokke@liferay.com>
2016-11-01 14:55:47 -0700

    method-scraper - adds getApiPath method

--- src/scraper/method-scraper.js
+++ src/scraper/method-scraper.js
@@ -2,12 +2,17 @@
 
 import BaseScraper from './base-scraper';
 
+const SELECTOR_API_PATH: string = '.lfr-api-method > h2';
 const SELECTOR_RETURN_TYPE: string = '.lfr-api-return-type .lfr-api-param-name';
 const SELECTOR_PARAMETERS: string = '.lfr-api-parameters';
 const SELECTOR_PARAMETER_NAMES: string = `${SELECTOR_PARAMETERS} .lfr-api-param-name`;
 const SELECTOR_PARAMETER_TYPES: string = `${SELECTOR_PARAMETERS} .lfr-api-param-type`;
 
 export default class MethodScraper extends BaseScraper {
+	getApiPath(): string {
+		return this.getTextFromSelector(SELECTOR_API_PATH);
+	}
+
 	getParameterNamesArray(): string[] {
 		return this.getArrayFromSelector(SELECTOR_PARAMETER_NAMES);
 	}
